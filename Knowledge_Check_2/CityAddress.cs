a578cdfab6e3cba31bf0832a25be50ea4aae8fc1
Jack Baylor <jackc.baylor@gmail.com>
2024-04-26 10:09:19 -0400

    I did it!

---
+++ Knowledge_Check_2/CityAddress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Check_2
+{
+    internal class CityAddress : StateAddress
+    {
+        public string CityName { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+    }
+}
--- Knowledge_Check_2/Program.cs
+++ Knowledge_Check_2/Program.cs
@@ -1,19 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Knowledge_Check_2;
+using System.Text.Json;
 
 Console.WriteLine("How many records do you want to add? ");
 var numberOfRecords = int.Parse(Console.ReadLine());
 
-var recordList = new List<MyClass>();
+var recordList = new List<CityAddress>();
 for (int i = 0; i < numberOfRecords; i++)
 {
     // In this loop, populate the object's properties using Console.ReadLine()
-    var myClass = new MyClass();
+    CityAddress address = new CityAddress();
 
-    Console.WriteLine("Enter the value for ");
-    myClass.myProperty = Console.ReadLine();
+    Console.WriteLine("Enter the values for your address.");
+    address.State = Console.ReadLine();
+    address.ZipCode = Console.ReadLine();
+    address.CityName = Console.ReadLine();
+    address.StreetName = Console.ReadLine();
+    address.StreetNumber = Console.ReadLine();
 
-    recordList.Add(myClass);
+    recordList.Add(address);
+    recordList.ToString();
+    
+    
 }
-
+    
 // Print out the list of records using Console.WriteLine()
+
+Console.WriteLine($"{JsonSerializer.Serialize(recordList)} has been added.");
+
+Console.ReadKey();
